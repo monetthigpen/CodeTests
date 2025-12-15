38d867688f176ada3e5591d949b8b39402f72222
Monet Thigpen <monet.thigpen@gmail.com>
2025-12-15 19:07:34 +0000

    test

--- codeTest2.tsx
+++ codeTest2.tsx
@@ -1,33 +1,36 @@
+// -------------------- Get SPUserIDs from PeoplePicker selection --------------------
 const getUserIdsFromSelection = React.useCallback(async (): Promise<number[]> => {
   const ids: number[] = [];
+  console.log(ids);
 
-  // Track Graph requests
-  let GrphIndex = 1;
+  // let batchFlag = false;
   let batchFlag = false;
-  const requestUri: any[] = [];
-  const keyValues: any[] = [];
 
-  // Loop through selected options
+  // Get context.pageContext.web.title.peoplePickerIDs object from local storage by key.
+  // make const localStorageVar
+  // let GrphIndex = 1;
+  // const requestUri: any = [];
+  const localStorageVar = `${context.pageContext.web.title}.peoplePickerIDs`;
+  let GrphIndex = 1;
+  const requestUri: any[] = []; // eslint-disable-line @typescript-eslint/no-explicit-any
+
+  // -------------------- Loop through selected options --------------------
   for (const e of selectedOptions) {
+    const elm = selectedOptionsRaw.filter((v) => v.DisplayText === e)[0];
 
-    // Find the matching entity from selectedOptionsRaw
-    const elm = selectedOptionsRaw.filter(
-      (v) => v.DisplayText === e
-    )[0];
+    // const item = [];
+    // filter elm through local storage using displayText const CheckSPUserIDStorage
+    const item: any[] = []; // eslint-disable-line @typescript-eslint/no-explicit-any
 
-    if (!elm) {
-      continue;
-    }
-
-    // ---------------------------------------------
-    // Filter elm through local storage using displayText / Key
-    // ---------------------------------------------
+    // filter elm through local storage using displayText const CheckSPUserIDStorage
     const checkSPUserIDStorage = localStorage.getItem(elm.Key);
     console.log(checkSPUserIDStorage);
 
-    // If checkSPUserIDStorage length > 0
-    // that means value is in local storage so no API call needed
-    if (checkSPUserIDStorage != null) {
+    // If checkSPUserIDStorage.length > 0 that means value is in local storage so no api call needed.
+    if (checkSPUserIDStorage != null && checkSPUserIDStorage.length > 0) {
+      // Get SPUserId
+      // const num = Number(checkSPUserIDStorage.SPUserID)
+      // push num to ids
       console.log("ids found");
 
       const num = Number(checkSPUserIDStorage);
@@ -36,84 +39,88 @@ const getUserIdsFromSelection = React.useCallback(async (): Promise<number[]> =>
       }
     }
 
-    // ---------------------------------------------
-    // Else: value not found in local storage
-    // Get values from selected options raw
-    // ---------------------------------------------
+    // else
+    // Get the values of the Key from Selected options raw
     else {
-
-      // Get the values of the Key from selected options raw
-      const key = elm.Key;
-      const displayText = elm.DisplayText;
-      const email = elm.EntityData?.Email;
-
       // Add the key values and displayText and GraphIndex and email to keyValues[]
+      // use index from keyValues[] for graphapi
+      const keyValues: any[] = []; // eslint-disable-line @typescript-eslint/no-explicit-any
+
       keyValues.push({
-        Key: key,
-        DisplayText: displayText,
-        Email: email,
-        GraphIndex: GrphIndex
+        Key: elm.Key,
+        DisplayText: elm.DisplayText,
+        GraphIndex: GrphIndex,
+        Email: elm.EntityData?.Email,
       });
 
-      // Use index from keyValues[] for graph API
-      const item = {
+      // item.push(
+      //   {
+      //     id: GrphIndex++,
+      //     method: 'GET',
+      //     url: `${context.pageContext.web.absoluteUrl}/_api/web/siteusers/getByEmail('${elm.EntityData?.Email}')`
+      //   }
+      // );
+      item.push({
         id: GrphIndex++,
         method: "GET",
-        url: `${context.pageContext.site.id}/_api/web/siteusers/getbyemail('${email}')`
-      };
+        url: `${context.pageContext.web.absoluteUrl}/_api/web/siteusers/getByEmail('${elm.EntityData?.Email}')`,
+      });
 
-      // Push graph request item
-      requestUri.push(item);
+      // requestUri.push(...item);
+      requestUri.push(...item);
     }
   }
 
-  // ---------------------------------------------
   // If requestUri.length > 0
-  // Create a batch call using graphAPI
-  // ---------------------------------------------
   if (requestUri.length > 0) {
+    // Create a batch call using graphAPI:
+    // let urlElm;
+    let urlElm: any; // eslint-disable-line @typescript-eslint/no-explicit-any
 
-    let urlElm: any;
-
+    // if (requestUri.length > 1) {
     if (requestUri.length > 1) {
-      const $batch = {
-        requests: requestUri
-      };
-
+      // const $batch = { requests: requestUri };
+      // urlElm = $batch;
+      // batchFlag = true;
+      const $batch = { requests: requestUri };
       urlElm = $batch;
       batchFlag = true;
     }
 
-    // Single request (no batch)
+    // else {
     else {
+      // urlElm = requestUri[0].url;
       urlElm = requestUri[0].url;
-      batchFlag = false;
     }
 
-    // Execute Graph API request
-    await makeGraphAPI(urlElm, batchFlag, keyValues);
-  }
+    // await makeGraphAPI(urlElm, batchFlag, keyValues)
+    await makeGraphAPI(urlElm, batchFlag, localStorageVar); // NOTE: you still need to implement makeGraphAPI
 
-  // ---------------------------------------------
-  // Store batch API results
-  // Compare keyValues[] and filter through the PPBatchResults
-  // ---------------------------------------------
-  const PPBatchResults = keyValues;
+    // store batch api results in const PPLBatchResults
+    // compare keyValues[] and filter through the PPLBatchResults
+    // const num = Number(elm.EntityData?.SPUserID)
+    // if (!Number.isNaN(num) && num > 0) {
+    //   ids.push(num);
+    // }
+    const PPLBatchResults = localStorage.getItem(localStorageVar);
 
-  for (const elm of PPBatchResults) {
-    const num = Number(elm.EntityData?.SPUserID);
+    if (PPLBatchResults) {
+      const parsed = JSON.parse(PPLBatchResults);
 
-    if (!Number.isNaN(num) && num > 0) {
-      ids.push(num);
+      // parsed is expected to be an array of entities with EntityData.SPUserID
+      for (const elm of parsed) {
+        const num = Number(elm?.EntityData?.SPUserID);
+
+        if (!Number.isNaN(num) && num > 0) {
+          ids.push(num);
+        }
+      }
     }
   }
 
-  // ---------------------------------------------
-  // Return collected SharePoint User IDs
-  // ---------------------------------------------
+  // else- return ids;
   return ids;
-
-}, [selectedOptions, selectedOptionsRaw]);
+}, [selectedOptions, selectedOptionsRaw, context]);
 
 
 
