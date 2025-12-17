473f697769dfc4a2a80f559e4268bdf86d659af8
Monet Thigpen <monet.thigpen@gmail.com>
2025-12-17 04:00:28 +0000

    new

--- codeTest2.tsx
+++ codeTest2.tsx
@@ -1,6 +1,5 @@
-url: `${context.pageContext.web.absoluteUrl}/_api/web/siteusers/getByEmail('${elm.EntityData?.Email ?? ""}')?$select=Id`
-
-kv.EntityData = {
-  ...(kv.EntityData ?? {}),
-  SPUserID: String(user.Id)
-};
+item.push({
+  id: GrphIndex++,
+  method: "GET",
+  url: `${conText.pageContext.web.absoluteUrl}/_api/web/lists(guid'fe8fcb98-439f-4f47-af7c-ce27c61d945a')/items?$expand=fields&$filter=fields/Title eq '${keyValues[0]?.DisplayText}'`
+});
