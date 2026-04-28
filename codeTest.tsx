5dc715f9339931d4742e57c99d40a388e77dce30
Monet Thigpen <monet.thigpen@gmail.com>
2026-04-28 17:28:02 +0000

    updates

--- codeTest.tsx
+++ codeTest.tsx
@@ -1,12 +1,13 @@
-const element: React.ReactElement<IAuthorizedRequestorProps> =
-  React.createElement(AuthorizedRequestor, {
-    context: this.context,
-    displayMode: this.displayMode,
-    onSave: this._onSave,
-    onClose: this._onClose
-  });
+public render(): void {
+  const element: React.ReactElement<any> = React.createElement(
+    AuthorizedRequestor as any,
+    {
+      context: this.context,
+      displayMode: this.displayMode,
+      onSave: this._onSave,
+      onClose: this._onClose
+    }
+  );
 
-ReactDOM.render(element, this.domElement);
-
-import AuthorizedRequestor from "./components/AuthorizedRequestor";
-import { IAuthorizedRequestorProps } from "./components/IAuthorizedRequestorProps";
+  ReactDOM.render(element, this.domElement);
+}
